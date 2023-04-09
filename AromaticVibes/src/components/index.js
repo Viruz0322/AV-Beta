58c6242348a4c89d2c4691f9a9f47cd69d05587f
francisco perez <frperez032219@gmail.com>
2023-04-08 18:28:25 -0700

    initial commit

---
+++ AromaticVibes/src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Stats,
+    Testimonials,
+    Footer,
+};
